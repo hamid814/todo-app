49aecbd43af9f866f0e4da9efba06866367bbc61
hamid <hamid331994@gmail.com>
2019-08-30 00:58:57 +0430

    Updates

---
+++ precache-manifest.3054440dfcb906d09448f5b2be374f57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1510bac11c864dd6b4f70c07db12c5c3",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "a0cdc5ba236464ae5cf2",
+    "url": "/todo-app/static/css/main.f04a8488.chunk.css"
+  },
+  {
+    "revision": "50fc83fdd3ed45f7a388",
+    "url": "/todo-app/static/js/2.21b042c9.chunk.js"
+  },
+  {
+    "revision": "a0cdc5ba236464ae5cf2",
+    "url": "/todo-app/static/js/main.09645fa7.chunk.js"
+  },
+  {
+    "revision": "22a810c513893e7bf031",
+    "url": "/todo-app/static/js/runtime~main.5cdee4a4.js"
+  }
+]);
